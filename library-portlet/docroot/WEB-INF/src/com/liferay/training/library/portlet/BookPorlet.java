f8d5fd359a6cab310cbab5c2305db8ef2876eb37
Eduardo Bohrer <nbluisrs@gmail.com>
2011-02-17 10:40:23 -0200

    first commit, first portlets

---
+++ library-portlet/docroot/WEB-INF/src/com/liferay/training/library/portlet/BookPorlet.java
@@ -0,0 +1,10 @@
+package com.liferay.training.library.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class BookPorlet
+ */
+public class BookPorlet extends MVCPortlet {
+
+}
---
+++ library-portlet/docroot/WEB-INF/src/com/liferay/training/library/portlet/PublisherPortlet.java
@@ -0,0 +1,10 @@
+package com.liferay.training.library.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PublisherPortlet
+ */
+public class PublisherPortlet extends MVCPortlet {
+
+}
